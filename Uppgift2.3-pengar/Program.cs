71c230b1264c02791dcb2ec13dccfcdc5b596d37
BigOreo25 <alts07001@utb.vaxjo.se>
2024-10-09 14:54:32 +0200

    Add project files.

---
+++ Uppgift2.3-pengar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace hoppa
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hej,tack för att ni väljer Krebs bilar. jag gissar på att du vill hyra en bil?");
+            string svar = Console.ReadLine();
+            Console.WriteLine("bra. hur många dagar vill du ha bil?");
+            int dagar = int.Parse(Console.ReadLine());
+            Console.WriteLine("coolt. hur många km kommer du köra ungefär?");
+            int km = int.Parse(Console.ReadLine());
+            int pris = 300 + (1 * km) + ((dagar - 1) * 500);
+            Console.WriteLine("här. pris = " + pris + "kr är vad det kommer kosta dig att få vår bil under en liten tid :)");
+            
+
+
+
+        }
+
+    }
+
+}
